bde5aae8fd71ca4859f6b3ad1ed1b661900b757d
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-07-21 21:29:31 +0200

    accept command line arguments as UCI input

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "uci/uci.hpp"
+#include <sstream>
 
-int main() {
+int main(int argv, char **argc) {
+    std::stringstream cli_arg_stream;
+    for (int i = 1; i < argv; ++i) {
+        cli_arg_stream << argc[i] << '\n'; 
+    }
+    uci::handler.process_input(cli_arg_stream, std::cout);
     uci::handler.process_input(std::cin, std::cout);
 }
