c78af411db6433308b2339d6274564d14e981843
JonathanHallstrom <lmj.hallstrom@gmail.com>
2025-07-22 14:30:55 +0200

    add isready support
    bench: 3024384

--- src/uci/uci.cpp
+++ src/uci/uci.cpp
@@ -78,6 +78,8 @@ void Handler::process_input(std::istream &in, std::ostream &out) {
             out << "id author Aron Petkovski, Jonathan Hallström" << std::endl;
             out << options;
             out << "uciok" << std::endl;
+        } else if (parts[0] == "isready") {
+            out << "readyok" << std::endl;
         } else if (parts[0] == "perft") {
             handle_perft(out, *util::parse_int(parts[1]));
         } else if (parts[0] == "print") {
