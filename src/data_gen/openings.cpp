7b0b47590737fbe3e17c40dbab0b1532d2f14cba
aronpetkovski <aronpetko@gmail.com>
2025-08-18 18:28:20 -0500

    Bench: 1118233

--- src/data_gen/openings.cpp
+++ src/data_gen/openings.cpp
@@ -4,24 +4,44 @@
 namespace datagen {
 
 BoardState generate_opening(usize random_moves) {
-    Board board(STARTPOS_FEN);
-    bool success;
-    do {
-        board = Board(STARTPOS_FEN);
-        success = true;
+    rng::seed_generator();
 
-        for (usize ply = 0; ply < random_moves; ++ply) {
+    const auto pick_random_move_count = [&]() { return random_moves; };
+
+    Board board(STARTPOS_FEN);
+    while (true) {
+        board.undo_n_moves(board.history().size() - 1);
+
+        const auto random_move_count = pick_random_move_count();
+
+        auto generated_successfully = true;
+        for (usize i = 0; i <= random_move_count; ++i) {
             MoveList moves;
             generate_moves(board.state(), moves);
-
             if (moves.empty() || board.is_fifty_move_draw() || board.has_threefold_repetition()) {
-                success = false;
+                generated_successfully = false;
                 break;
             }
 
-            board.make_move(moves[rng::next_u64(0, moves.size() - 1)]);
+            // Generated enough moves and didn't reach a terminal node
+            if (i == random_move_count) {
+                break;
+            }
+
+            util::StaticVector<std::pair<Move, i32>, 218> scored_moves;
+            for (const auto move : moves) {
+                scored_moves.push_back({move, rng::next_u64(-100, 100)});
+            }
+
+            const auto move = std::max_element(std::begin(scored_moves), std::end(scored_moves),
+                                               [](auto &lhs, auto &rhs) { return lhs.second < rhs.second; });
+            board.make_move(move->first);
         }
-    } while (!success);
+
+        if (generated_successfully) {
+            break;
+        }
+    }
 
     return board.state();
 }
