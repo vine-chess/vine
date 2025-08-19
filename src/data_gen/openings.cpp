2d11397ba48cd7ed579912de6b044f92b8253f8a
aronpetkovski <aronpetko@gmail.com>
2025-08-18 19:04:32 -0500

    Bench: 1118233

--- src/data_gen/openings.cpp
+++ src/data_gen/openings.cpp
@@ -1,48 +1,33 @@
 #include "openings.hpp"
 #include "../chess/move_gen.hpp"
-#include <algorithm>
 
 namespace datagen {
 
 BoardState generate_opening(usize random_moves) {
-    rng::seed_generator();
-
-    const auto pick_random_move_count = [&]() { return random_moves; };
-
     Board board(STARTPOS_FEN);
-    while (true) {
-        board.undo_n_moves(board.history().size() - 1);
+    bool success;
+    do {
+        board = Board(STARTPOS_FEN);
+        success = true;
 
-        const auto random_move_count = pick_random_move_count();
-
-        auto generated_successfully = true;
-        for (usize i = 0; i <= random_move_count; ++i) {
+        for (usize ply = 0; ply < random_moves; ++ply) {
             MoveList moves;
             generate_moves(board.state(), moves);
+
             if (moves.empty() || board.is_fifty_move_draw() || board.has_threefold_repetition()) {
-                generated_successfully = false;
+                success = false;
                 break;
             }
 
-            // Generated enough moves and didn't reach a terminal node
-            if (i == random_move_count) {
-                break;
-            }
-
-            util::StaticVector<std::pair<Move, i32>, 218> scored_moves;
-            for (const auto move : moves) {
-                scored_moves.push_back({move, rng::next_u64(-100, 100)});
-            }
-
-            const auto move = std::max_element(std::begin(scored_moves), std::end(scored_moves),
-                                               [](auto &lhs, auto &rhs) { return lhs.second < rhs.second; });
-            board.make_move(move->first);
+            board.make_move(moves[rng::next_u64(0, moves.size() - 1)]);
         }
 
-        if (generated_successfully) {
-            break;
+        MoveList moves;
+        generate_moves(board.state(), moves);
+        if (moves.empty() || board.is_fifty_move_draw() || board.has_threefold_repetition()) {
+            success = false;
         }
-    }
+    } while (!success);
 
     return board.state();
 }
