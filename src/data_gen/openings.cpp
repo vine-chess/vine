88e01302630836cbbd5424b6490186bde3fa817e
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-08-23 17:50:13 +0200

    Throw out imbalanched openings (#46)
    * Fix info printing
    
    * Fix compile error
    
    * Throw out imbalanced openings
    
    ---------
    
    Co-authored-by: aronpetko <aronpetko@gmail.com>
    Bench: 1006578

--- src/data_gen/openings.cpp
+++ src/data_gen/openings.cpp
@@ -1,9 +1,15 @@
 #include "openings.hpp"
 #include "../chess/move_gen.hpp"
+#include "../search/searcher.hpp"
+#include <iostream>
 
 namespace datagen {
 
 BoardState generate_opening(usize random_moves) {
+    search::Searcher searcher;
+    searcher.set_hash_size(4);
+    searcher.set_verbosity(search::Verbosity::NONE);
+
     Board board(STARTPOS_FEN);
     bool success;
     do {
@@ -22,11 +28,26 @@ BoardState generate_opening(usize random_moves) {
             board.make_move(moves[rng::next_u64(0, moves.size() - 1)]);
         }
 
-        MoveList moves;
-        generate_moves(board.state(), moves);
-        if (moves.empty() || board.is_fifty_move_draw() || board.has_threefold_repetition()) {
-            success = false;
-        }
+        const auto is_opening_valid = [&]() {
+            // Opening ends in a terminal state
+            MoveList moves;
+            generate_moves(board.state(), moves);
+            if (moves.empty() || board.is_fifty_move_draw() || board.has_threefold_repetition()) {
+                return false;
+            }
+
+            // Position is too imbalanced
+            searcher.go(board, {.max_depth = 5, .max_iters = 1000});
+            const auto cp_score =
+                static_cast<i32>(std::round(-400.0 * std::log(1.0 / searcher.game_tree().root().q() - 1.0)));
+            if (std::abs(cp_score) >= 300) {
+                return false;
+            }
+
+            return true;
+        };
+
+        success = is_opening_valid();
     } while (!success);
 
     return board.state();
