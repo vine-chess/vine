a72eafb96b72c51a1cbb1cc82443b84975e82c24
Aron Petkovski <aronpetko@gmail.com>
2025-09-10 02:16:02 -0500

    bench: 539907

--- src/uci/uci.cpp
+++ src/uci/uci.cpp
@@ -248,7 +248,7 @@ void Handler::process_input(std::istream &in, std::ostream &out) {
                 if (moves_pos != std::string::npos) {
                     std::istringstream moves_stream(line.substr(moves_pos + 7));
                     std::string move_str;
-                    while (moves_stream >> move_str) {z
+                    while (moves_stream >> move_str) {
                         board_.make_move(board_.create_move(move_str));
                     }
                 }
