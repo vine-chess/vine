c5cd3dfef0840f508a3035c349b9596f84e6328d
aronpetko <aronpetko@gmail.com>
2025-09-19 16:14:49 -0500

    Bench: 671119

--- src/search/game_tree.cpp
+++ src/search/game_tree.cpp
@@ -180,7 +180,7 @@ void GameTree::compute_policy(const BoardState &state, NodeIndex node_idx) {
         Node &child = node_at(node.first_child_idx + i);
         // Compute policy output for this move
         const auto &history_entry = butterfly_table_[board_.state().side_to_move][child.move.from()][child.move.to()];
-        child.policy_score = (ctx.logit(child.move) + history_entry / 16384.0) / temperature;
+        child.policy_score = ctx.logit(child.move) / temperature + history_entry / 16384.0;
         // Keep track of highest policy so we can shift all the policy
         // values down to avoid precision loss from large exponents
         highest_policy = std::max(highest_policy, child.policy_score);
