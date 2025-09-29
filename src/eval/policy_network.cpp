e637f2c539fc3f2d121e37c15d72fe3b0b100d8b
aronpetko <aronpetko@gmail.com>
2025-09-28 21:14:05 -0500

    Bench: 671086

--- src/eval/policy_network.cpp
+++ src/eval/policy_network.cpp
@@ -98,7 +98,8 @@ f32 PolicyContext::logit(Move move) const {
     for (usize i = 0; i < L1_SIZE / VECTOR_SIZE; ++i) {
         const auto clamped =
             util::min_epi16<VECTOR_SIZE>(util::max_epi16<VECTOR_SIZE>(feature_accumulator_[i], zero), one);
-        sum += util::madd_epi16(clamped * clamped, util::convert_vector<i16, i8, VECTOR_SIZE>(network->l1_weights_vec[idx][i]));
+        sum += util::madd_epi16(clamped * clamped,
+                                util::convert_vector<i16, i8, VECTOR_SIZE>(network->l1_weights_vec[idx][i]));
     }
 
     const i32 dot = util::reduce_vector<i32, VECTOR_SIZE / 2>(sum);
