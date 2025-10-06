88bcae7ef3f00ef9dda5b7a8c0b70bc0ebd3b7cf
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-10-06 21:27:54 +0200

    less aggro?
    bench: 767324

--- src/search/game_tree.cpp
+++ src/search/game_tree.cpp
@@ -148,7 +148,7 @@ NodeIndex GameTree::select_and_expand_node() {
             // Scale the exploration constant logarithmically with the number of visits this node has
             base *= 1.0 + std::log((node.num_visits + CPUCT_VISIT_SCALE) / CPUCT_VISIT_SCALE_DIVISOR);
 
-            base *= 1 + (node.q_variance() - 0.005);
+            base *= 1 + node.q_variance() / 4;
             base *=
                 std::min<f64>(GINI_MAXIMUM, GINI_BASE - GINI_MULTIPLIER * std::log(node.gini_impurity / 255.0 + 0.001));
             // sum_var += base;
