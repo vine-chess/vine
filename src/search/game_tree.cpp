1b71b052095eb376a959fad3429fd42f525da0a0
aronpetkovski <aronpetko@gmail.com>
2026-02-11 18:28:02 -0600

    Bench: 568134

--- src/search/game_tree.cpp
+++ src/search/game_tree.cpp
@@ -326,7 +326,7 @@ void GameTree::backpropagate_score(f64 score) {
 
         // A node's score is the average of all of its children's score
         auto &node = node_at(node_idx);
-        const auto relaxation_factor = std::max(1.0 - static_cast<double>(node.num_visits) / 16384.0, 0.9);
+        const auto relaxation_factor = std::max(1.0 - static_cast<double>(node.num_visits) / 512.0, 0.9);
         node.sum_of_scores += score * relaxation_factor;
         node.num_visits++;
         hash_table_.update(board_.state().hash_key, node.q(), node.num_visits);
