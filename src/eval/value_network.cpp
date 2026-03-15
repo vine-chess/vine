6790ecbea5878ac6530074e520d41fb5f6a2dd37
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-03-15 18:52:20 +0100

    single layer 1024hl net
    bench: 800471

--- src/eval/value_network.cpp
+++ src/eval/value_network.cpp
@@ -1,6 +1,5 @@
 #include "value_network.hpp"
 
-#include <algorithm>
 #include <array>
 #include <cstring>
 
@@ -12,11 +11,9 @@ namespace detail {
 
 [[nodiscard]] const util::MultiArray<i16Vec, L1_SIZE / VECTOR_SIZE> &feature(Square sq, PieceType piece,
                                                                              Color piece_color, Color perspective,
-                                                                             Square king_sq, Bitboard threats,
-                                                                             Bitboard defences) {
+                                                                             Square king_sq) {
     usize flip = 0b111000 * perspective ^ 0b000111 * (king_sq.file() >= File::E);
-    return network
-        ->ft_weights_vec[defences.is_set(sq)][threats.is_set(sq)][piece_color != perspective][piece - 1][sq ^ flip];
+    return network->ft_weights_vec[piece_color != perspective][piece - 1][sq ^ flip];
 }
 
 } // namespace detail
@@ -27,103 +24,35 @@ f64 evaluate(const BoardState &state) {
 
     const auto stm = state.side_to_move;
     const auto king_sq = state.king(stm).lsb();
-
-    const std::array<Bitboard, 2> threats = {state.pinned_threats_by(Color::WHITE),
-                                             state.pinned_threats_by(Color::BLACK)};
-
     // Accumulate features for both sides, viewed from side-to-move's perspective
     for (PieceType piece = PieceType::PAWN; piece <= PieceType::KING; piece = PieceType(piece + 1)) {
         // Our pieces
         for (auto sq : state.piece_bbs[piece - 1] & state.occupancy(stm)) {
-            const auto feat = detail::feature(sq, piece, stm, stm, king_sq, threats[~stm], threats[stm]);
             for (usize i = 0; i < L1_SIZE / VECTOR_SIZE; ++i) {
-                accumulator[i] += feat[i];
+                accumulator[i] += detail::feature(sq, piece, stm, stm, king_sq)[i];
             }
         }
-
         // Opponent pieces
         for (auto sq : state.piece_bbs[piece - 1] & state.occupancy(~stm)) {
-            const auto feat = detail::feature(sq, piece, ~stm, stm, king_sq, threats[stm], threats[~stm]);
             for (usize i = 0; i < L1_SIZE / VECTOR_SIZE; ++i) {
-                accumulator[i] += feat[i];
+                accumulator[i] += detail::feature(sq, piece, ~stm, stm, king_sq)[i];
             }
         }
     }
 
-    const f32 dequantisation_constant = 1.0 / (QA * QA * QB);
+    util::SimdVector<i32, VECTOR_SIZE / 2> sum{};
+    const auto zero = util::set1<i16, VECTOR_SIZE>(0);
+    const auto one = util::set1<i16, VECTOR_SIZE>(QA);
 
-    const i16 *l1 = reinterpret_cast<const i16 *>(accumulator.data());
-
-    std::array<i32, L2_SIZE> l2_int{};
-    for (usize i = 0; i < L1_SIZE / 2 / L2_REG_SIZE; ++i) {
-        // Load register values for pairwise
-        auto left = util::loadu<i16, L2_REG_SIZE>(l1 + L2_REG_SIZE * i);
-        auto right = util::loadu<i16, L2_REG_SIZE>(l1 + L2_REG_SIZE * i + L1_SIZE / 2);
-
-        // Clamp to [0, 1] (quantized)
-        left = util::clamp_scalar<i16, L2_REG_SIZE>(left, 0, QA);
-        right = util::clamp_scalar<i16, L2_REG_SIZE>(right, 0, QA);
-
-        // Widen so pairwise doesnt overflow the i16s, using u16s here is neutral
-        const auto left_widened = util::convert_vector<u16, i16, L2_REG_SIZE>(left);
-        const auto right_widened = util::convert_vector<u16, i16, L2_REG_SIZE>(right);
-
-        // Pairwise multiply the clamped values
-        const auto activated = left_widened * right_widened;
-
-        //  Matrix multiply l1 -> l2
-        for (usize j = 0; j < L2_REG_SIZE; ++j) {
-            const auto idx = i * L2_REG_SIZE + j;
-            for (usize k = 0; k < L2_SIZE; ++k) {
-                l2_int[k] += activated[j] * network->l1_weights[idx][k];
-            }
-        }
+    for (usize i = 0; i < L1_SIZE / VECTOR_SIZE; ++i) {
+        const auto clamped = util::min<i16, VECTOR_SIZE>(util::max<i16, VECTOR_SIZE>(accumulator[i], zero), one);
+        sum += util::madd_epi16(clamped, clamped * network->l1_weights_vec[i]);
     }
 
-    std::array<f32, L2_SIZE> l2;
-    for (usize i = 0; i < L2_SIZE; ++i) {
-        l2[i] = l2_int[i] * dequantisation_constant + network->l1_biases[i];
-    }
-
-    // Activate l2
-    for (usize i = 0; i < L2_SIZE / L2_REG_SIZE; ++i) {
-        auto v = util::loadu<f32, L2_REG_SIZE>(l2.data() + L2_REG_SIZE * i);
-        const auto scaled = util::fma<f32, L2_REG_SIZE>(v, util::set1<f32, L2_REG_SIZE>(1.0f / 6.0f),
-                                                        util::set1<f32, L2_REG_SIZE>(0.5f));
-        v *= util::clamp_scalar<f32, L2_REG_SIZE>(scaled, 0, 1);
-        util::storeu<f32, L2_REG_SIZE>(l2.data() + L2_REG_SIZE * i, v);
-    }
-
-    std::array<f32, L3_SIZE> l3{};
-    for (usize i = 0; i < L3_SIZE / L3_REG_SIZE; ++i) {
-        auto v = util::loadu<f32, L3_REG_SIZE>(network->l2_biases.data() + L3_REG_SIZE * i);
-        auto g = util::loadu<f32, L3_REG_SIZE>(network->l2_biases.data() + L3_REG_SIZE * i + L3_SIZE);
-
-        // Matrix multiply l2 -> l3
-        for (usize j = 0; j < L2_SIZE; ++j) {
-            const auto l2_val = util::set1<f32, L3_REG_SIZE>(l2[j]);
-            const auto w1 = network->l2_weights_vec[j][i];
-            const auto w2 = network->l2_weights_vec[j][i + L3_SIZE / L3_REG_SIZE];
-            v = util::fma<f32, L3_REG_SIZE>(l2_val, w1, v);
-            g = util::fma<f32, L3_REG_SIZE>(l2_val, w2, g);
-        }
-
-        const auto g_scaled = util::fma<f32, L3_REG_SIZE>(g, util::set1<f32, L3_REG_SIZE>(1.0f / 6.0f),
-                                                          util::set1<f32, L3_REG_SIZE>(0.5f));
-        // Activate l3
-        v *= util::clamp_scalar<f32, L3_REG_SIZE>(g_scaled, 0, 1);
-
-        // Matrix multiply l3 -> out
-        const auto l3_val = util::loadu<f32, L3_REG_SIZE>(l3.data() + L3_REG_SIZE * i);
-        util::storeu<f32, L3_REG_SIZE>(l3.data() + L3_REG_SIZE * i,
-                                       util::fma<f32, L3_REG_SIZE>(v, network->l3_weights_vec[i], l3_val));
-    }
-
-    f32 final_sum = network->l3_biases[0];
-    for (usize i = 0; i < L3_SIZE; ++i) {
-        final_sum += l3[i];
-    }
-    return final_sum;
+    const i32 dot = util::reduce_vector<i32, VECTOR_SIZE / 2>(sum);
+    const i32 bias = network->l1_biases[0];
+    return (dot / static_cast<f64>(QA) + bias) / static_cast<f64>(QA * QB);
 }
 
 } // namespace network::value
+
--- src/eval/value_network.hpp
+++ src/eval/value_network.hpp
@@ -4,48 +4,29 @@
 #include "../chess/board_state.hpp"
 #include "../util/multi_array.hpp"
 #include "../util/simd.hpp"
-#include <algorithm>
+#include <array>
 
 namespace network::value {
 
 constexpr i16 QA = 255;
 constexpr i16 QB = 64;
-constexpr usize L1_SIZE = 4096;
-constexpr usize L2_SIZE = 16;
-constexpr usize L3_SIZE = 128;
-constexpr usize VECTOR_SIZE = util::NATIVE_SIZE<i16>;
-constexpr usize L2_REG_SIZE = std::min(util::NATIVE_SIZE<i16>, L2_SIZE);
-constexpr usize L3_REG_SIZE = std::min(util::NATIVE_SIZE<f32>, L3_SIZE);
-
+constexpr usize L1_SIZE = 1024;
+constexpr usize VECTOR_SIZE = std::min<usize>(L1_SIZE, util::NATIVE_SIZE<i16>);
 constexpr i16 EVAL_SCALE = 400;
 
 using i16Vec = util::SimdVector<i16, VECTOR_SIZE>;
-using i8Vec = util::SimdVector<i8, VECTOR_SIZE>;
 
 struct alignas(util::NATIVE_VECTOR_ALIGNMENT) ValueNetwork {
     union {
-        util::MultiArray<i16Vec, 2, 2, 2, 6, 64, L1_SIZE / VECTOR_SIZE> ft_weights_vec;
-        util::MultiArray<i16, 2, 2, 2, 6, 64, L1_SIZE> ft_weights;
+        util::MultiArray<i16Vec, 2, 6, 64, L1_SIZE / VECTOR_SIZE> ft_weights_vec;
+        util::MultiArray<i16, 2, 6, 64, L1_SIZE> ft_weights;
     };
-    util::MultiArray<i16, L1_SIZE> ft_biases;
-
+    std::array<i16, L1_SIZE> ft_biases;
     union {
-        util::MultiArray<util::SimdVector<i8, L2_REG_SIZE>, L1_SIZE / 2, L2_SIZE / L2_REG_SIZE> l1_weights_vec;
-        util::MultiArray<i8, L1_SIZE / 2, L2_SIZE> l1_weights;
+        util::MultiArray<i16Vec, L1_SIZE / VECTOR_SIZE> l1_weights_vec;
+        util::MultiArray<i16, L1_SIZE> l1_weights;
     };
-    util::MultiArray<f32, L2_SIZE> l1_biases;
-
-    union {
-        util::MultiArray<util::SimdVector<f32, L3_REG_SIZE>, L2_SIZE, L3_SIZE * 2 / L3_REG_SIZE> l2_weights_vec;
-        util::MultiArray<f32, L2_SIZE, L3_SIZE * 2> l2_weights;
-    };
-    util::MultiArray<f32, L3_SIZE * 2> l2_biases;
-
-    union {
-        util::MultiArray<util::SimdVector<f32, L3_REG_SIZE>, L3_SIZE / L3_REG_SIZE> l3_weights_vec;
-        util::MultiArray<f32, L3_SIZE> l3_weights;
-    };
-    util::MultiArray<f32, 1> l3_biases;
+    std::array<i16, 1> l1_biases;
 };
 
 f64 evaluate(const BoardState &state);
